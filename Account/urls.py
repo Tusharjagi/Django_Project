98994e75a34d04888cb4f08b7e60bfd104726804
Tusharjagi <tusharjagi@gmail.com>
2022-02-26 00:52:01 +0530

    Project

---
+++ Account/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('register',views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name="logout")
+]
+
---
+++ Tango/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('', include('travello.urls')),
+    path('admin/', admin.site.urls),
+    path('account/', include('Account.urls'))
+
+]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
